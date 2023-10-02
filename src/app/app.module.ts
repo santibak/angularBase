290e60e09dac6cdcf7644956bdae90b803f77e56
Karoll Santibanez Caceres <ksantibane.entersoft@bancochile.cl>
2023-10-01 22:53:46 -0300

    Fin seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HeroModule } from './heroes/heroes.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
